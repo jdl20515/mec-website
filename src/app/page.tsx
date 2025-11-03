06ba79d3b4db5cc713711e973202271d83410c08
David Lomelin <83227279+jdl20515@users.noreply.github.com>
2025-11-03 18:32:27 -0500

    actual init

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between">
+        <h1 className="text-center mb-8">
+          Welcome to MEC Website
+        </h1>
+        <p className="text-center text-gray-600">
+          Under construction...
+        </p>
+      </div>
+    </main>
+  );
+}
